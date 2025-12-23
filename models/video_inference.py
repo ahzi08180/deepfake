18bbc42db789e5a98828653b5dfd2f81ad25daf9
Ahzi <acs110119@gm.ntcu.edu.tw>
2025-12-24 01:59:26 +0800

    fix: 影片取樣時間

--- models/video_inference.py
+++ models/video_inference.py
@@ -2,13 +2,7 @@ import cv2
 import numpy as np
 from PIL import Image
 
-def predict_video(video_path, face_detector, image_model, frame_interval=15):
-    """
-    video_path: 影片路徑
-    face_detector: MTCNN 版本 FaceDetector
-    image_model: DeepfakeImageModel
-    frame_interval: 每隔幾幀抽取一次
-    """
+def predict_video(video_path, face_detector, image_model, frame_interval=5):
     cap = cv2.VideoCapture(video_path)
     probs = []
     idx = 0
@@ -19,13 +13,15 @@ def predict_video(video_path, face_detector, image_model, frame_interval=15):
             break
 
         if idx % frame_interval == 0:
-            # 將 BGR -> RGB -> PIL
             img_pil = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
             faces = face_detector.detect_faces_image(img_pil)
 
-            # detect_faces_image 返回單張裁切人臉 (numpy array)
             if faces is not None:
-                probs.append(image_model.predict(faces))
+                for face in faces:
+                    # resize 保證 224x224
+                    face = cv2.resize(face, (224,224))
+                    probs.append(image_model.predict(face))
+
         idx += 1
 
     cap.release()
