ad967253850f9d16a987f64f3758b6ac7d6ccd5b
Ahzi <acs110119@gm.ntcu.edu.tw>
2025-12-24 01:43:39 +0800

    fix: 移除bounding box

--- app.py
+++ app.py
@@ -26,30 +26,19 @@ if file:
         # 讀取圖片
         img_array = np.frombuffer(file.read(), np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
-
-        # 將 BGR 轉 RGB，給 Streamlit 顯示
-        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        st.image(img[:, :, ::-1])
 
         # 將 OpenCV BGR 轉 PIL.Image
-        img_pil = Image.fromarray(img_rgb)
+        img_pil = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 
         # 使用 MTCNN 偵測人臉
-        faces = face_detector.mtcnn.detect(img_pil)
-        if faces is None:
+        face = face_detector.detect_faces_image(img_pil)
+        if face is None:
             st.error("No face detected.")
         else:
-            # 在原圖上畫 bounding box
-            for box in faces[0]:
-                x1, y1, x2, y2 = map(int, box)
-                cv2.rectangle(img_rgb, (x1, y1), (x2, y2), (0, 0, 255), 2)  # 紅色框
-
-            st.image(img_rgb)  # 顯示帶框的圖片
-
-            # # 取第一張臉裁切做推論
-            # x1, y1, x2, y2 = map(int, faces[0])
-            # face_crop = img_rgb[y1:y2, x1:x2]
-            p = float(image_model.predict(faces))
-            st.success(f"Fake Probability: {p:.2f}")
+            # 推論
+            p = float(image_model.predict(face))
+            st.success(f"Fake Probability: {p*100:.2f}%")
             st.progress(p)
 
     else:  # 影片
@@ -64,5 +53,5 @@ if file:
         if p is None:
             st.error("No face detected.")
         else:
-            st.success(f"Fake Probability: {p*100: .2f}%")
+            st.success(f"Fake Probability: {p:.2f*100}%")
             st.progress(p)
